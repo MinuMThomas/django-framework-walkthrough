c5cb374d3749494a4c3d2e9b0e2514eb98bcd128
MinuMThomas <minumarytomas@gmail.com>
2022-12-02 16:42:28 +0000

    Modified add item.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from env import S_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ph8=!m#$-7$ojwqf1byfzk5atsjr1k4$eb8+33fbp2)497@@+6'
+SECRET_KEY = 'S_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
